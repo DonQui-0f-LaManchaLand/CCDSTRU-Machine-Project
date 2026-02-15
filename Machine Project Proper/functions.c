18c4a4b688e43a71e9e9ffdce2d673518f1b2d67
rachellecheng-droid <rachelle_cheng@dlsu.edu.ph>
2026-02-15 12:56:51 +0000

    data types are subject to change and extra functions go in helper

---
+++ Machine Project Proper/functions.c
@@ -0,0 +1,37 @@
+#include "helper.c"
+
+/*
+ */
+void Remove(){
+
+}
+
+/*
+*/
+void Replace(){
+
+}
+
+/*
+*/
+void Expand(){
+
+}
+
+/*
+*/
+void Update(){
+
+}
+
+/*
+*/
+void NextPlayerMove(){
+
+}
+
+/*
+*/
+void GameOver(){
+    
+}
